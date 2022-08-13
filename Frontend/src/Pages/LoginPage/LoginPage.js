e980cb708c07674fb72dfc8ac3e344caf6aac44f
Bonie Sachdev <bonysachdev99@gmail.com>
2022-08-14 04:18:44 +0530

    Updated login screen functionality

--- Frontend/src/Pages/LoginPage/LoginPage.js
+++ Frontend/src/Pages/LoginPage/LoginPage.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginPage.css";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import MainScreen from "../../Components/MainScreen/MainScreen";
 import LottieDisplay from "../../Components/LottieDisplay/LottieDisplay";
 import loginPerson from "../../assets/lottie/loginPerson";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import ErrorMessage from "../../Components/ErrorMessage/ErrorMessage";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "api/users/login",
+        {
+          email,
+          password,
+        },
+        config
+      );
+
+      console.log(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
   return (
     <Container>
       <MainScreen MainScreen title="LOGIN">
@@ -18,14 +47,25 @@ const LoginPage = () => {
           </Col>
           <Col>
             <div className="loginContainer">
-              <Form>
+              {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+              <Form onSubmit={submitHandler}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                   Submit
